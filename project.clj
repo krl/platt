ffba50435b51074abbb154029fe54d2b0f3c7561
Kristoffer Ström <kristoffer@rymdkoloni.se>
2013-09-04 14:30:23 +0200

    version downbump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject platt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject platt "0.0.1"
+  :description "Clojure interface to the java port of leveldb"
+  :url "https://github.com/krl/platt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure      "1.5.1"]
